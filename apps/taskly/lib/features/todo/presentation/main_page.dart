e54bc389287d6bb91499ca1f9ce3917acfb1533b
onishi taisei <taisei.developer@gmail.com>
2023-10-22 19:30:25 +0900

    🚀 feat: BottomNavigationBarの実装

--- apps/taskly/lib/features/todo/presentation/main_page.dart
+++ apps/taskly/lib/features/todo/presentation/main_page.dart
@@ -1,7 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
 
-class MainPage extends StatelessWidget {
+part 'main_page.g.dart';
+
+@riverpod
+class TabIndexState extends _$TabIndexState {
+  @override
+  int build() => 0;
+
+  void switchTab(int index) => state = index;
+}
+
+class MainPage extends ConsumerWidget {
   const MainPage({super.key});
+
+  static const _pages = [
+    Text('タスク'),
+    Text('設定'),
+  ];
 
   static const _items = [
     BottomNavigationBarItem(
@@ -15,12 +32,22 @@ class MainPage extends StatelessWidget {
   ];
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
+      // TODO(taisei): 共通のAppBarを作成する
+      appBar: AppBar(title: Text('タスク')),
+      body: Center(
+        child: IndexedStack(
+          index: ref.watch(tabIndexStateProvider),
+          children: _pages,
+        ),
+      ),
       bottomNavigationBar: BottomNavigationBar(
-        currentIndex: 0,
-        onTap: (value) {},
         items: _items,
+        currentIndex: ref.watch(tabIndexStateProvider),
+        onTap: (index) =>
+            ref.read(tabIndexStateProvider.notifier).switchTab(index),
       ),
     );
   }
---
+++ apps/taskly/lib/features/todo/presentation/main_page.g.dart
@@ -0,0 +1,26 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'main_page.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$tabIndexStateHash() => r'b68c54e240ca704df051de59b99e9c540865f7fe';
+
+/// See also [TabIndexState].
+@ProviderFor(TabIndexState)
+final tabIndexStateProvider =
+    AutoDisposeNotifierProvider<TabIndexState, int>.internal(
+  TabIndexState.new,
+  name: r'tabIndexStateProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$tabIndexStateHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$TabIndexState = AutoDisposeNotifier<int>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
