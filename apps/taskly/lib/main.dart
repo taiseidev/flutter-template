af42365fbafa43adc9d6b45245e4a8678e73a0e6
onishi taisei <taisei.developer@gmail.com>
2023-10-22 19:33:28 +0900

    🚜 refactor: ディレクトリ構成の変更

--- apps/taskly/lib/main.dart
+++ apps/taskly/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:taskly/features/todo/presentation/app.dart';
+import 'package:taskly/app.dart';
 
 void main() {
   runApp(
